eef439eaa060858907d9406c404ad72c552a84ad
anasMARZAK <76627567+anasMARZAK@users.noreply.github.com>
2021-02-22 21:54:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -393,7 +393,7 @@ if (message.content === '$spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 3000000000000; x++) {
-        message.channel.send(`**??? ???? ??? **[ " ${x} " ]`)
+        message.channel.send(`**??? ???? ??? **[ " ${FDGFGFDFx} " ]`)
           .then(m => {
             count++;
           })
