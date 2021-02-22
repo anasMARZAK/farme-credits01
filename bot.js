f0866b9d8ae406262e7664cdaf34b8ac0b9b9f7b
anasMARZAK <76627567+anasMARZAK@users.noreply.github.com>
2021-02-22 23:37:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -420,7 +420,7 @@ client3.on('message', message => {
 if (message.content === '$spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 30000; x++) {
+      for(let x = 0; x < 30000000000; x++) {
         message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
           .then(m => {
             count++;
