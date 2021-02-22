3c44e72b839ef05230d9859374abf4972f29d773
anasMARZAK <76627567+anasMARZAK@users.noreply.github.com>
2021-02-22 21:55:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -389,7 +389,7 @@ message.channel.send('#credits')
 
 
 client.on('message', message => {
-if (message.content === '$spam') {
+if (message.content === '/spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 3000000000000; x++) {
